b874c9a8e58b782e91e4fcc4983d6dbec75fd0fd
Michael Caldwell <michaelc@coastalvectors.com>
2016-04-04 09:18:17 -0700

    Added files via upload
    Initial check in for tip.py. Script will open the test image and print it to the screen using PIL library.

---
+++ tip.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import Image, sys
+# resize an image using the PIL image library
+
+# open test image file
+# (.bmp,.jpg,.png,.gif supported by PIL)
+imageFile = "TestImage.jpg"
+img = Image.open(imageFile)
+
+# adjust width and height to your needs - 32 MAX
+maxSize = 64
+width = img.size[0]
+height = img.size[1]
+
+if width > height:
+    ratio = int(width/maxSize)
+else:
+    ratio = int(height/maxSize)
+
+width = int(width / ratio)
+height =int(height / ratio)
+
+img = img.resize((width, height), Image.ANTIALIAS)
+
+pixels = img.load()
+for y in range(0, img.size[1], 2):
+    for x in range(img.size[0]):
+        r, g, b = pixels[x, y]
+        backgroundCode = (16 + (36 * int(r/51)) + (6 * int(g/51)) + int(b/51))
+
+        r, g, b = pixels[x, y+1]
+        forgroundCode = (16 + (36 * int(r/51)) + (6 * int(g/51)) + int(b/51))
+
+        #sys.stdout.write("%3d;%3d;%3d %x\n" % (r, g, b, colorCode)) # DEBUG
+        sys.stdout.write("\x1b[48;5;%d;38;5;%dm%s\x1b[0m" % (backgroundCode, forgroundCode, u"\u2584"))
+    print("")
+
